c332efcf33e18bb02898c8120675dc17760d111f
mjfox33 <mjfox33@gmail.com>
2022-02-15 14:37:20 -0500

    reworked the code to use a prefix sum matrix

--- code_1314_matrix_block_sum.py
+++ code_1314_matrix_block_sum.py
@@ -3,13 +3,28 @@ class Solution:
         n = len(mat)
         m = len(mat[0]) # assuming all rows have the same number of columns
         result = [ [0]*m for _ in range(n)]
+
+        # build prefix_sum matrix
+        prefix_sum = [ [0]*m for _ in range(n)]
         for row in range(n):
             for col in range(m):
-                for row_delta in range(-k, k+1, 1):
-                    for col_delta in range(-k, k+1, 1):
-                        row_index = row + row_delta
-                        col_index = col + col_delta
-                        if  row_index < 0 or row_index > n-1 or col_index < 0 or col_index > m-1:
-                            continue
-                        result[row][col] += mat[row+row_delta][col+col_delta]
+                prefix_sum[row][col] += mat[row][col]
+                if col > 0:
+                    prefix_sum[row][col] += prefix_sum[row][col-1]
+                if row > 0:
+                    prefix_sum[row][col] += prefix_sum[row-1][col]
+                if col > 0 and row > 0:
+                    prefix_sum[row][col] -= prefix_sum[row-1][col-1]
+
+        # now calculate values for each cell using prefix sum matrix
+        for row in range(n):
+            for col in range(m):
+                result[row][col] = prefix_sum[min(n-1, row+k)][min(m-1, col+k)]
+                if row > k:
+                    result[row][col] -= prefix_sum[row-k-1][min(m-1, col+k)]
+                if col > k:
+                    result[row][col] -= prefix_sum[min(n-1, row+k)][col-k-1]
+                if row > k and col > k:
+                    result[row][col] += prefix_sum[row-k-1][col-k-1] 
+
         return result
